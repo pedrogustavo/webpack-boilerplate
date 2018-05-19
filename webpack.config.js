c1683bbe36c1ce7381b59125b5eaafe94c1020c1
Pedro Gustavo <eu@pedrogustavo.com>
2018-05-19 21:57:02 +0000

    :wrench: Basic configuration for html-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,19 @@
-const path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var path = require('path');
 
 module.exports = {
   entry: './src/scripts/index.js',
+
+  mode: 'production',
+
   output: {
-    filename: 'bundle.js',
+    filename: 'assets/bundle.js',
     path: path.resolve(__dirname, 'dist')
-  }
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      inject: true
+    })
+  ]
 };
